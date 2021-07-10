137be125ef9e8b68c6947adce3b7fede0762c8ed
paulMolano <paulmolanoo@gmail.com>
2021-07-10 15:03:40 +0200

    changeImageJS

---
+++ app.js
@@ -0,0 +1,15 @@
+let imagePrincipal = document.getElementById("img2");
+let but2 = document.getElementById("but2");
+
+// but2.addEventListener("click", changeImage);
+
+// function changeImage() {
+//     imagePrincipal.src =
+//         "https://images.pexels.com/photos/8647586/pexels-photo-8647586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+
+// TODO IMAGEN 2 https://images.pexels.com/photos/8647586/pexels-photo-8647586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+
+but2.addEventListener("click", () => {
+  imagePrincipal.src =
+    "https://images.pexels.com/photos/8647586/pexels-photo-8647586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+});
