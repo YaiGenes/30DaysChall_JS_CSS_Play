c184b5a623babf2c2321fff0bd93eb1e7bc636ca
paulMolano <paulmolanoo@gmail.com>
2021-07-17 12:12:34 +0200

    listeners

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ let but2 = document.getElementById("but2");
 //         "https://images.pexels.com/photos/8647586/pexels-photo-8647586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
 
 // TODO IMAGEN 2 https://images.pexels.com/photos/8647586/pexels-photo-8647586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
-
+but1.addEventListener("click", () => {
+  imagePrincipal.src =
+    "https://images.pexels.com/photos/6407742/pexels-photo-6407742.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260";
+});
 but2.addEventListener("click", () => {
   imagePrincipal.src =
     "https://images.pexels.com/photos/8647586/pexels-photo-8647586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+});
+but3.addEventListener("click", () => {
+  imagePrincipal.src =
+    "https://images.unsplash.com/photo-1626483639770-016fc6e65ce6?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
 });
